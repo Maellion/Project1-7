1b910e65a92c706c75b9f131beaebae3445d28c5
Maellion <1029462@hr.nl>
2023-09-08 14:48:13 +0200

    update 0.3

--- Opdracht 3.py
+++ Opdracht 3.py
@@ -1,1 +1,31 @@
-dit werkt nu
+def loop1():
+    wifi_str = input("Welke wifi wilt u gebruiken? [4G, 5G, Wifi open] ").lower()
+
+    if wifi_str == "4g":
+        print("U bent verbonden met 4G")
+
+    elif wifi_str == "5g":
+        print("U bent verbonden met 5G")
+
+    elif wifi_str == "wifi open":
+        print("U heeft voor de Wifi open gekozen, wij wijzen u erop dat de data door de eigenaar van dit netwerk is te lezen.")
+        loop2()
+
+    else:
+        print("Kies een geldige optie")
+        loop1()
+
+def loop2():
+    open_wifi_str = input("Weet U zeker dat u wilt verbinden met Wifi open? [Ja/Nee] ").lower()
+
+    if open_wifi_str == "ja":
+        print("U bent verbonden met Wifi open")
+
+    elif open_wifi_str == "nee":
+        print("U bent niet verbonden")
+
+    else:
+        print("Kies een geldige optie")
+        loop2()
+
+loop1()
