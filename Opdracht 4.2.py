962e75d65b177fe9f3ef96f29e8e4dcf9b5b8de7
Maellion <1029462@hr.nl>
2023-09-15 14:49:32 +0200

    update 0.5

---
+++ Opdracht 4.2.py
@@ -0,0 +1,18 @@
+computer_suppliers = ("Apple", "Asus", "Dell", "Lenovo", "Acer", "Samsung", "MSI", "Hewlett-Packard (HP)", "Toshiba", "Microsoft", "Chuwi", "Sony")
+
+print(computer_suppliers)
+print()
+
+number_of_computer_suppliers = len(computer_suppliers)
+print(f"De tuple bevat {number_of_computer_suppliers} computer leveranciers.")
+print(computer_suppliers)
+print()
+
+computer_supplier = computer_suppliers[7]
+characters_in_name = len(computer_supplier)
+print(f"De naam van {computer_supplier} bestaat uit {characters_in_name} karakters.")
+print()
+
+index_of_computer_supplier = computer_suppliers.index("Samsung")
+print(f"De index van computer leverancier {computer_suppliers[index_of_computer_supplier]} is {index_of_computer_supplier}.")
+print()
--- Opdracht 4.py
+++ Opdracht 4.py
@@ -11,3 +11,21 @@ my_list.append(6)
 # ...of op een specifieke locatie
 my_list.insert(3, "three")
 print(my_list)
+
+# Create a tuple
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+# Tuples zijn niet wijzigbaar
+# my_tuple[0] = 6
+
+
+# Dictionary - zorgt ervoor dat je een waarde geeft aan gene wat voor de : staat
+dictionary = {"key": "value"}
+print(dictionary)
+print(dictionary["key"])
+# Dit geeft een nieuwe waarde aan de eerdere dictionary
+dictionary["key"] = "new value"
+print(dictionary["key"])
+# Voegt de nieuwe waarde toe aan de oude value, en print die achter elkaar
+dictionary["new key"] = "new value"
+print(dictionary)
